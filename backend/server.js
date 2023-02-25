09e6256995da2b54244bda9939f4045607794342
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-25 16:31:25 +0100

    🎨 random updates

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const morgan=require('morgan');
+require('dotenv').config();
+
+
+app.use(morgan('dev'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,7 +17,6 @@ app.post('/', (req, res) => {
     const data = req.body;
     res.send(`Hello ${data.name}!`);
 });
-const PORT=3000;
-app.listen(PORT, () => {
-    console.log('Server running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Server running on port '+process.env.PORT);
 });
