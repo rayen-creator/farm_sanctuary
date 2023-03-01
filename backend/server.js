4ca161cf5e80c68ed3cd83985bc9fee94d0fcb70
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-01 17:23:32 +0100

    🔥 update server

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
 const app = express();
-const morgan=require('morgan');
-require('dotenv').config();
+const bodyParser = require("body-parser");
+const { ApolloServer, gql } = require("apollo-server-express");
+const morgan = require("morgan");
+require("dotenv").config();
+const typeDefs=require('./src/schema/schema.graphql')
+const mongodbconnection = require("./src/db/index");
+const userResolver = require("./src/resolvers/user.resolver");
 
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-    const data = req.body;
-    res.send(`Hello ${data.name}!`);
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: [
+    userResolver
+],
 });
-app.listen(process.env.PORT, () => {
-    console.log('Server running on port '+process.env.PORT);
+async function startApolloServer() {
+    await server.start();
+    server.applyMiddleware({ app });
+  }
+  
+  startApolloServer();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
