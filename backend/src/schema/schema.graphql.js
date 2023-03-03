ce3b9437d03a8600014223e1a291f57efbee5cac
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-03 18:44:13 +0100

    ✅ update graphql schema

--- backend/src/schema/schema.graphql.js
+++ backend/src/schema/schema.graphql.js
@@ -1,44 +1,52 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 const typeDefs = gql`
+  scalar DateTime
 
-scalar DateTime
-
-enum Role {
-  FARMER
-  CLIENT
-  ADMIN
-}
-
+  enum Role {
+    FARMER
+    CLIENT
+    ADMIN
+  }
 
   type User {
-  id: ID!
-  username: String!
-  email: String!
-  password: String!
-  createdAt: DateTime!
-  updatedAt: DateTime!
-  isActive: Boolean!
-  isBlocked: Boolean!
-  role: Role!
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    isActive: Boolean!
+    isBlocked: Boolean!
+    role: Role!
   }
-  
+
   input UserInput {
-  username: String!
-  email: String!
-  password: String!
-  isActive: Boolean!
-  role: Role!
-}
+    username: String!
+    email: String!
+    password: String!
+    isActive: Boolean!
+    role: Role!
+  }
 
-
-
+  input signinInput {
+    email: String!
+    password: String!
+  }
+  type LoginResponse {
+    accessToken: String!
+    username: String!
+    message: String!
+    expiresIn: Int!
+  }
   type Query {
     getUser(id: ID!): User!
     getUsers: [User!]!
   }
 
   type Mutation {
-    createUser(input: UserInput!): User!
+    signup(input: UserInput!): User!
+    signin(input: signinInput!): LoginResponse!
+
     updateUser(id: ID!, input: UserInput!): User!
     deleteUser(id: ID!): User!
   }
