1288e6a4b161336c69e45399c4236e4ee4a6ba78
ghaith <ghaith.benrzig@esprit.tn>
2023-03-04 19:45:32 +0100

    user list table

--- frontend/src/app/components/backoffice/backoffice.module.ts
+++ frontend/src/app/components/backoffice/backoffice.module.ts
@@ -4,13 +4,15 @@ import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { ClockComponent } from './shared/clock/clock.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
     BackofficeComponent,
     SidebarComponent,
-    ClockComponent    
+    ClockComponent,
+    UserListComponent    
   ],
   imports: [
     CommonModule,
