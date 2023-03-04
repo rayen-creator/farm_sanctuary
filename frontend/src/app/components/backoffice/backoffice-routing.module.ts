1ea7dd492938f2d350639a45e45602b6f5b388a9
ghaith <ghaith.benrzig@esprit.tn>
2023-03-04 19:45:32 +0100

    user list table

--- frontend/src/app/components/backoffice/backoffice-routing.module.ts
+++ frontend/src/app/components/backoffice/backoffice-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackofficeComponent } from './backoffice.component';
+import {UserListComponent} from "./users/user-list/user-list.component";
 
 const routes: Routes = [
   {
     path: '', component: BackofficeComponent, children: [
-
+      {path:'users', component: UserListComponent}
     ]
   }
 ];
