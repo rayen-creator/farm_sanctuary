1186e70ce8a6176382a9c3a562f7913a33d8489e
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-03-04 20:55:33 +0100

    initial commit

---
+++ backend/src/resolvers/feedback.resolver.js
@@ -0,0 +1,60 @@
+const { UserInputError } = require('apollo-server-express');
+const feedbackService = require('../services/feedback');
+
+const feedbackResolver = {
+
+  Query: {
+    async getFeedback(_, { id }) {
+      try {
+        const feedback = await feedbackService.getFeedback(id);
+        if (!feedback) {
+          throw new UserInputError('Feedback not found');
+        }
+        return feedback;
+      } catch (error) {
+        throw new UserInputError(error.message);
+      }
+    },
+    async getFeedbacks() {
+      try {
+        return await feedbackService.getFeedbacks();
+      } catch (error) {
+        throw new UserInputError(error.message);
+      }
+    },
+  },
+
+  Mutation: {
+    async createFeedback(_, { input }) {
+      try {
+        return await feedbackService.createFeedback(input);
+      } catch (error) {
+        throw new UserInputError(error.message);
+      }
+    },
+    async updateFeedback(_, { id, input }) {
+      try {
+        const feedback = await userService.updateFeedback(id, input);
+        if (!feedback) {
+          throw new UserInputError('Feedback not found');
+        }
+        return user;
+      } catch (error) {
+        throw new UserInputError(error.message);
+      }
+    },
+    async deleteFeedback(_, { id }) {
+      try {
+        const user = await feedbackService.deleteFeedback(id);
+        if (!user) {
+          throw new UserInputError('Feedback not found');
+        }
+        return user;
+      } catch (error) {
+        throw new UserInputError(error.message);
+      }
+    },
+  },
+};
+
+module.exports = feedbackResolver;
