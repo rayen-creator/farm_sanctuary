5d5975c43ea562d02072fe11f619bee8080f3b35
ghaith <ghaith.benrzig@esprit.tn>
2023-03-04 20:33:41 +0100

    graphql shit

--- frontend/src/app/components/backoffice/users/user-list/user-list.component.ts
+++ frontend/src/app/components/backoffice/users/user-list/user-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {users} from "../../../../graphql/graphql.queries";
+import {Apollo, gql} from "apollo-angular";
+import {User} from "../../../../core/models/user";
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
-  constructor() { }
+userList: User[];
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
-  }
+    this.apollo
+      .watchQuery({
+        query: users
+      }).valueChanges.subscribe({
+      next: (data: any) => {
+        this.userList = data.data;
+console.log(this.userList)
+      },
+      error: (err) => {
+        console.log("err :" + err)
 
+      }
+    })
+  }
 }
