3c3dfc40d8cb35a97229c277e3a34353dab7cc86
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-05 23:58:34 +0100

    💄 added auth components

---
+++ frontend/src/app/components/frontoffice/auth/updatepwd/updatepwd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-updatepwd',
+  templateUrl: './updatepwd.component.html',
+  styleUrls: ['./updatepwd.component.css']
+})
+export class UpdatepwdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
