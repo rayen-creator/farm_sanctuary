58c905047a0b5d9d4d920ddb6d8ed7a8e18a28ca
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-07 23:34:13 +0100

    ✨ update login component

--- frontend/src/app/components/frontoffice/auth/login/login.component.ts
+++ frontend/src/app/components/frontoffice/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/core/models/user';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Customvalidator } from 'src/app/core/utils/custom-validator';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginform: FormGroup
+  constructor(private authservice:AuthService,private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginform = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
+
+  Valid(controlname: any, loginform: any) {
+    return Customvalidator.Valid(controlname, loginform)
+
+  }
+  login(form: User) {
+    if (form.email != "" && form.password != "") {
+      this.authservice.login(form)
+      console.log("Login form", form);
+
+    } else {
+      Customvalidator.validateAllFormFields(this.loginform);
+    }
+  }
+
 
 }
