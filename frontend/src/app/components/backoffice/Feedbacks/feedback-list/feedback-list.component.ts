a0d552c18bd83d8237ba4951227773c856246f47
NourMakdouli <99952554+NourMakdouli@users.noreply.github.com>
2023-03-08 19:16:17 +0100

    badge correction

--- frontend/src/app/components/backoffice/Feedbacks/feedback-list/feedback-list.component.ts
+++ frontend/src/app/components/backoffice/Feedbacks/feedback-list/feedback-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
+import { Apollo, gql } from 'apollo-angular';
+import { rating} from 'src/app/core/models/rating';
+
 import { Feedback } from 'src/app/core/models/feedback';
 import { FeedbackService } from 'src/app/core/services/feedback.service';
 import { feedbacks } from 'src/app/graphql/graphql.queries.feedback';
@@ -12,13 +14,30 @@ import { feedbacks } from 'src/app/graphql/graphql.queries.feedback';
 export class FeedbackListComponent implements OnInit {
   feedbackList: Feedback [];
   public data:any;
+  public listlength:number=0;
+
   public order:boolean=false;
   public isDesc:boolean=false;
+  public rating = rating;
+
+  pagedItems: Feedback[];
+  filteredItems: Feedback[] ;
+  pageSize = 2;
+  currentPage = 1;
+  totalPages: number;
+  pages: number[];
+
+  searchText = '';
+
+
+
+
 
   constructor(private apollo: Apollo, private feedbackService: FeedbackService) { }
 
   ngOnInit(): void { 
     this.getFeedbacks();
+    this.listlength=this.feedbackList.length;
   }
 
 
@@ -40,15 +59,37 @@ export class FeedbackListComponent implements OnInit {
   }
 
 
-  sortTitle(title:any)
-  {this.feedbackService.sortString(this.feedbackList,title)}
+  setPage(page: number): void {
+    this.currentPage = page;
+    const startIndex = (page - 1) * this.pageSize;
+    let endIndex = startIndex + this.pageSize;
+
+ 
+    // Update total pages based on filtered user list
+    this.totalPages = Math.ceil(this.feedbackList.length / this.pageSize);
+    this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+
+    // Adjust end index if last page contains only admin users
+    if (this.currentPage === this.totalPages && endIndex > this.feedbackList.length) {
+      endIndex = this.feedbackList.length;
+    }
+
+    this.pagedItems = this.feedbackList.slice(startIndex, endIndex);
+    this.filteredItems = this.pagedItems;
+  }
+
+
+  onSearch(): void {
+    const query = this.searchText.trim().toLowerCase();
+    if (query) {
+      this.filteredItems = this.feedbackList.filter(feedback =>
+        feedback.title.toLowerCase().includes(query) || feedback.content.toLowerCase().includes(query) || feedback.subject.toLowerCase().includes(query)
+      );
+    } else {
+      this.filteredItems = this.pagedItems;
+    }
+  }
   
-  sortSubject(subject:any)
-  {this.feedbackService.sortString(this.feedbackList,subject)}
-  
-  sortRating(rating:any)
-  {this.feedbackService.sortString(this.feedbackList,rating)}
-  
-  sortCategory(title:any)
-  {this.feedbackService.sortString(this.feedbackList,title)}
+
+
 }
