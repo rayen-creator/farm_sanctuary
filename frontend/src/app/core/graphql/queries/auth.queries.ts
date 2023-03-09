60188f668f249ca82d8ebfa4a1841ba60eeb8a4a
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-09 23:52:12 +0100

    🔥 update graphql structure

---
+++ frontend/src/app/core/graphql/queries/auth.queries.ts
@@ -0,0 +1,40 @@
+import { gql } from "apollo-angular";
+
+export const login = gql`
+mutation signin($input: signinInput!) {
+  signin(input: $input) {
+    accessToken
+    username
+    message
+    expiresIn
+  }
+}
+`;
+
+export const sendmail = gql`
+mutation sendmail($input: ForgetpwdInput!) {
+  sendmail(input: $input) {
+    message
+    mailstatus
+  }
+}
+`;
+
+export const checkresettoken=gql`
+mutation checkresettoken($input: checkresettoken!) {
+  checkresettoken(input: $input) {
+    valid
+    message
+  }
+}
+`;
+
+export const resetpwd = gql`
+mutation resetpwd($input: signinInput!) {
+  resetpwd(input: $input) {
+    message
+    updateStatus
+    userFound
+  }
+}
+`;
