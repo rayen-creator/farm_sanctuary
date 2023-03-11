822e3610efdeaf0d48db9267331c506416ec7c01
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-12 00:07:23 +0100

    ✅ update header with current username

--- frontend/src/app/components/frontoffice/shared/header/header.component.ts
+++ frontend/src/app/components/frontoffice/shared/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  username: string | null;
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.userIsAuthenticated = this.auth.isUserAuth();
+    this.authListenerSubs = this.auth.getAuthStatusListener().subscribe(
+      (isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+      }
+    )
+    this.username = this.auth.getUsername();
+
+
   }
 
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
+  }
+  loggingout() {
+    this.auth.logout();
+  }
 }
