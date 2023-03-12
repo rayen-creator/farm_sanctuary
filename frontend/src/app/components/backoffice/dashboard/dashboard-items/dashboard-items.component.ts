16e32c8293322e2ac6cf3f402d656ce4d5eb7932
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-03-13 00:14:49 +0100

    dashboard

--- frontend/src/app/components/backoffice/dashboard/dashboard-items/dashboard-items.component.ts
+++ frontend/src/app/components/backoffice/dashboard/dashboard-items/dashboard-items.component.ts
@@ -5,19 +5,15 @@ import { User } from 'src/app/core/models/user';
 import { roles } from 'src/app/core/models/role';
 import { genders } from 'src/app/core/models/gender';
 
-
 import {
   ChartComponent,
   ApexAxisChartSeries,
   ApexChart,
   ApexXAxis,
   ApexTitleSubtitle,
-  ApexOptions
-  
-} from "ng-apexcharts";
-import { UserCount } from 'src/app/core/models/userCount';
-
-
+  ApexOptions,
+} from 'ng-apexcharts';
+import { UserCount } from 'src/app/core/graphql/userCount';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -28,136 +24,234 @@ export type ChartOptions = {
 
 @Component({
   selector: 'app-admin-dashboard',
-  templateUrl: "./dashboard-items.component.html",
-  styleUrls: ['./dashboard-items.component.css']
+  templateUrl: './dashboard-items.component.html',
+  styleUrls: ['./dashboard-items.component.css'],
 })
 export class AdminDashboardComponent implements OnInit {
   userCountsByDate: UserCount[];
 
+  @ViewChild('chart') chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions> |any;
+  public chartOptionss: Partial<ChartOptions> |any;
+  public ChartOptions: Partial<ChartOptions> |any;
 
+
+  adminList: User[] = [];
+  userList: User[] = [];
+  numberOfAdmins: Number;
+  numberOfarmers: Number;
+  numberOfClients: Number;
+  numberOfMaleClients: Number;
+  numberOfFemalClients: Number;
+  numberOfFemaleFarmers: Number;
+  numberOfMaleFarmers: Number;
+  numberOfActiveUsers: Number;
+  numberOfNonActiveUsers: Number;
+
+ 
   
-  @ViewChild("chart") chart: ChartComponent;
-  public chartOptions: Partial<ChartOptions>;
-
-
-  userList: User[] = []
-  numberOfAdmins : Number
-  numberOfarmers : Number
-  numberOfClients : Number
-  numberOfMaleClients : Number
-  numberOfFemaleFarmers : Number
-  numberOfMaleFarmers : Number
-  numberOfActiveUsers : Number
-  numberOfNonActiveUsers : Number
-  numberOfFemalClients : Number
-  adminList : User[] =[]
   date: string;
   count: number;
+
+  
+  
+  constructor(private apollo: Apollo) {
+   
+}
+
   
 
-  // ChartOptions:ApexOptions  = {
-  //   series:[
-  //     {
-  //       name: "My-series",
-  //       data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-  //     }
-  //   ],
-  //   chart: {
-  //     height: 350,
-  //     type: "bar"
-  //   },
-  //   title : {
-  //     text: "My First Angular Chart"
-  //   },
-  //   xaxis: {
-  //     categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"],
-  //     title: {
-  //       text: "Month"
-  //     }
-  //   }
-  // };
-  constructor(private apollo : Apollo) {
-   
-  }
-   
-
-
-  ngOnInit(): void{
+  ngOnInit(): void {
     
     this.UsersCount();
     this.UsersCountByCategory();
     this.UserCountByDate();
-    
+
+
+   
+
+    console.log(this.numberOfFemaleFarmers)
   }
 
-  UserCountByDate(){
-    this.apollo.watchQuery({
-      query: users,
-    }).valueChanges.subscribe((result: any) => {
-      const userCountsByDate: { [date: string]: number } = {};
-      for (const user of result.data.getUsers) {
-        const date = user.createdAt.split('T')[0];
-        userCountsByDate[date] = (userCountsByDate[date] || 0) + 1;
-      }
-      const userCountsByDateArray = [];
-      for (const date in userCountsByDate) {
-        if (userCountsByDate.hasOwnProperty(date)) {
-          userCountsByDateArray.push({ date, count: userCountsByDate[date] });
+  UserCountByDate() {
+    this.apollo
+      .watchQuery({
+        query: users,
+      })
+      .valueChanges.subscribe((result: any) => {
+        const userCountsByMonth: { [month: string]: number } = {};
+        for (const user of result.data.getUsers) {
+          const month = user.createdAt.split('T')[0].substring(0, 7);
+          userCountsByMonth[month] = (userCountsByMonth[month] || 0) + 1;
+
+        const userCountsByDateArray = [];
+        for (const date in userCountsByMonth) {
+          if (userCountsByMonth.hasOwnProperty(date)) {
+            userCountsByDateArray.push({ date, count: userCountsByMonth[date] });
+          }
         }
-      }
-      userCountsByDateArray.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-      this.userCountsByDate = userCountsByDateArray;
-    });
+        userCountsByDateArray.sort(
+          (a, b) => new Date(a.date + "-01").getMonth() - new Date(b.date + "-01").getMonth()
+          );
+          
+        this.userCountsByDate = userCountsByDateArray;
+
+        }
+        
+
+     
+        const counts = this.userCountsByDate.map(item => item.count);
+        const dates = this.userCountsByDate.map(item => item.date);
+          this.chartOptionss = {
+          series: [
+            {
+              name: "number of users created by date",
+              data: counts
+
+            }
+          ],
+          chart: {
+            height: 350,      
+            type: "bar"
+          },
+          title: {
+            text: "My Chart Title",
+            style: {
+              fontSize: "20px",
+              fontWeight: "bold",
+              color: "green",
+              fontFamily: "Arial, sans-serif",
+            
+              // Add any other styles you want to apply to the title here
+            
+          }},
+          xaxis: {
+            typ:'datetime',
+            categories: dates
+          }
+      
+        };
+      });
   }
 
-
-  
-
-
-  UsersCount(){
+  UsersCount() {
     this.apollo
       .watchQuery({
         query: users,
       })
       .valueChanges.subscribe({
-      next: (result: any) => {
-        this.userList = result.data.getUsers as User[];
-        console.log(this.userList);
-        
-      },
-      error: (err) => {
-        console.log("err :" + err);
-      },
-    });
+        next: (result: any) => {
+          this.userList = result.data.getUsers as User[];
+          console.log(this.userList);
+        },
+        error: (err) => {
+          console.log('err :' + err);
+        },
+      });
   }
 
-  UsersCountByCategory(){
+  UsersCountByCategory() {
     this.apollo
       .watchQuery({
         query: users,
       })
       .valueChanges.subscribe({
-      next: (result: any) => {
-        this.adminList = result.data.getUsers.filter((user: User) => user.role === roles.ADMIN);
-        this.numberOfAdmins = this.adminList.length;     
-        this.adminList = result.data.getUsers.filter((user:User)=> user.role === roles.CLIENT && user.gender === genders.FEMALE )  
-        this.numberOfFemalClients = this.adminList.length; 
-        this.adminList = result.data.getUsers.filter((user: User) => user.role === roles.FARMER);
-        this.numberOfarmers = this.adminList.length; 
-        this.adminList = result.data.getUsers.filter((user:User)=> user.role === roles.CLIENT && user.gender === genders.MALE )  
-        this.numberOfMaleClients = this.adminList.length; 
-        this.adminList = result.data.getUsers.filter((user:User)=> user.role === roles.FARMER && user.gender === genders.FEMALE )  
-        this.numberOfFemaleFarmers = this.adminList.length; 
-        this.adminList = result.data.getUsers.filter((user:User)=> user.role === roles.FARMER && user.gender === genders.MALE )  
-        this.numberOfMaleFarmers = this.adminList.length; 
-        this.adminList = result.data.getUsers.filter((user: User) => user.isActive === true);
-        this.numberOfActiveUsers = this.adminList.length;  
-        this.adminList = result.data.getUsers.filter((user: User) => user.isActive === false);
-        this.numberOfNonActiveUsers = this.adminList.length;  
-        
-        
-        
-        
+        next: (result: any) => {
+          this.adminList = result.data.getUsers.filter(
+            (user: User) => user.role === roles.ADMIN
+          );
+          this.numberOfAdmins = this.adminList.length;
+          this.adminList = result.data.getUsers.filter(
+            (user: User) =>
+              user.role === roles.CLIENT && user.gender === genders.FEMALE
+          );
+          this.numberOfFemalClients = this.adminList.length;
+          this.adminList = result.data.getUsers.filter(
+            (user: User) => user.role === roles.FARMER
+          );
+          this.numberOfarmers = this.adminList.length;
+          this.adminList = result.data.getUsers.filter(
+            (user: User) =>
+              user.role === roles.CLIENT && user.gender === genders.MALE
+          );
+          this.numberOfMaleClients = this.adminList.length;
+          this.adminList = result.data.getUsers.filter(
+            (user: User) =>
+              user.role === roles.FARMER && user.gender === genders.FEMALE
+          );
+          this.numberOfFemaleFarmers = this.adminList.length;
+          this.adminList = result.data.getUsers.filter(
+            (user: User) =>
+              user.role === roles.FARMER && user.gender === genders.MALE
+          );
+          this.numberOfMaleFarmers = this.adminList.length;
+          this.adminList = result.data.getUsers.filter(
+            (user: User) => user.isActive === true
+          );
+          this.numberOfActiveUsers = this.adminList.length;
+            console.log(this.numberOfActiveUsers)
+
+          this.adminList = result.data.getUsers.filter(
+            (user: User) => user.isActive === false
+          );
+          this.numberOfNonActiveUsers = this.adminList.length;
+         
+
+          this.adminList = result.data.getUsers.filter(
+            (user: User) => user.role === roles.CLIENT
+          );
+          this.numberOfClients = this.adminList.length;
+
+
+          
+          this.chartOptions = {
+            series: [this.numberOfMaleClients, this.numberOfFemalClients],
+            chart: {
+              width: 380,
+              type: "pie"
+            },
+            title: {
+              text: "Nomber of Client by gender"
+            },
+            labels: ["male", "female"],
+            responsive: [
+              {
+                breakpoint: 480,
+                options: {
+                  chart: {
+                    width: 200
+                  },
+                  legend: {
+                    position: "bottom"
+                  }
+                }
+              }
+            ]
+          };
+          this.ChartOptions = {
+            series: [this.numberOfMaleFarmers, this.numberOfFemaleFarmers],
+            chart: {
+              width: 380,
+              type: "pie"
+            },
+            title: {
+              text: "Nomber of Farmer by gender"
+            },
+            labels: ["male farmer", "female farmer"],
+            responsive: [
+              {
+                breakpoint: 480,
+                options: {
+                  chart: {
+                    width: 200
+                  },
+                  legend: {
+                    position: "bottom"
+                  }
+                }
+              }
+            ]
+          };
 
           // this.adminList.filter(user => user.role === roles.FARMER);
           // this.numberOfarmers = this.adminList.filter.length;
@@ -165,16 +259,11 @@ export class AdminDashboardComponent implements OnInit {
           // this.numberOfClients = this.adminList.filter.length;
           // this.adminList.filter(user => user.role === roles.CLIENT && user.gender === genders.MALE)
           // this.numberOfFemalClients = this.adminList.filter.length;
-     
-        console.log(this.numberOfAdmins);
-        
-      },
-      error: (err) => {
-        console.log("err :" + err);
-      },
-    });
-  }
 
-
-  
+        },
+        error: (err) => {
+          console.log('err :' + err);
+        },
+      });
   }
+}
--- frontend/src/app/core/models/user.ts
+++ frontend/src/app/core/models/user.ts
@@ -1,20 +1,20 @@
-import { roles } from "./role";
-import {genders} from "./gender";
+import { roles } from './role';
+import { genders } from './gender';
 
 export type User = {
-    'id': String;
-    'username': String
-  'phone': Number
-    'email': String
-    'password': String
-    'isActive': Boolean
-  'createdAt': Date
-  'updatedAt': Date
-  'isBlocked': Boolean
-  'gender': genders
-    'role': roles
-  'image': {
-    'url': string;
-    'contentType': string;
+  id: String;
+  username: String;
+  phone: Number;
+  email: String;
+  password: String;
+  isActive: Boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  isBlocked: Boolean;
+  gender: genders;
+  role: roles;
+  image: {
+    url: string;
+    contentType: string;
   };
-}
+};
