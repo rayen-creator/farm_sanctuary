b4638304df971c05b0ae543b36a4d8d3fbdfdb48
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-14 00:52:09 +0100

    ✅ update 2FA component

--- frontend/src/app/components/frontoffice/auth/two-factorauth/two-factorauth.component.ts
+++ frontend/src/app/components/frontoffice/auth/two-factorauth/two-factorauth.component.ts
@@ -1,8 +1,10 @@
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { Customvalidator } from 'src/app/core/utils/custom-validator';
 
 @Component({
   selector: 'app-two-factorauth',
@@ -10,27 +12,42 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./two-factorauth.component.css']
 })
 export class TwoFactorauthComponent implements OnInit {
-  username:string="nour";
-  verifyOTPForm = new FormGroup({
-    firstNumber: new FormControl('', Validators.required),
-    secondNumber: new FormControl('', Validators.required),
-    thirdNumber: new FormControl('', Validators.required),
-    fourthNumber: new FormControl('', Validators.required)
-  });
+  username: string;
+  verifyOTPForm: FormGroup
   verificationCode: string = '';
 
-  constructor( private authservice:AuthService,private formBuilder: FormBuilder) { }
+  constructor(private authservice: AuthService, private formBuilder: FormBuilder,private toaster:ToastrService) { }
 
   ngOnInit(): void {
-   
+    this.verifyOTPForm = this.formBuilder.group({
+      firstNumber: ['', Validators.required],
+      secondNumber: ['', Validators.required],
+      thirdNumber: ['', Validators.required],
+      fourthNumber: ['', Validators.required]
+    });
+    this.username = this.authservice.getUsername();
   }
-  
 
-onSubmit(){
-  const { firstNumber, secondNumber, thirdNumber, fourthNumber } = this.verifyOTPForm.value;
-  this.verificationCode = `${firstNumber}${secondNumber}${thirdNumber}${fourthNumber}`;
-  console.log('Verification code:', this.verificationCode); 
-  this.authservice.verifyOTP(this.username,this.verificationCode);
-  
-}
+  Valid(controlname: any, verifyOTPForm: any) {
+    return Customvalidator.Valid(controlname, verifyOTPForm)
+
+  }
+
+  onSubmit(form: any) {
+    const { firstNumber, secondNumber, thirdNumber, fourthNumber } = form;
+    const verificationCode = `${firstNumber}${secondNumber}${thirdNumber}${fourthNumber}`;
+    
+    console.log('Verification code:', form);
+    console.log('Verification code:', verificationCode.toString());
+
+    if (form.firstNumber != "" || form.secondNumber != "" || form.thirdNumber != "" || form.fourthNumber != "") {
+      this.authservice.verifyOTP(this.username, verificationCode.toString());
+    } else {
+      this.toaster.error('please insert your code to continue !','Error',{
+        progressBar :true
+      })
+      Customvalidator.validateAllFormFields(this.verifyOTPForm);
+
+    }
+  }
 }
