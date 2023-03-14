5cb75c22cb93bd420afbbf905ae9d9c7b497b1fc
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-03-14 22:30:57 +0100

    ✅ update profile and header

--- frontend/src/app/components/frontoffice/shared/header/header.component.ts
+++ frontend/src/app/components/frontoffice/shared/header/header.component.ts
@@ -1,58 +1,65 @@
 import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { DecodedToken } from 'src/app/core/graphql/graphqlResponse/decodedToken';
 import { AuthService } from 'src/app/core/services/auth.service';
+import jwt_decode from "jwt-decode";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy  {
-  username: string ;
-  role:string ;
+export class HeaderComponent implements OnInit, OnDestroy {
+  username: string;
+  role: string;
   userIsAuthenticated = false;
+  token:string;
+  decodedToken :DecodedToken;
+  userId:string;
   private authListenerSubs: Subscription;
-  private usernameSubs : Subscription;
-  private roleSubs :Subscription;
+  private usernameSubs: Subscription;
+  private roleSubs: Subscription;
   constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
     this.userIsAuthenticated = this.auth.isUserAuth();
     this.authListenerSubs = this.auth.getAuthStatusListener().subscribe({
-      next :(isAuthenticated) => {
+      next: (isAuthenticated) => {
         this.userIsAuthenticated = isAuthenticated;
-        
+
       },
-      error :()=>{
+      error: () => {
         this.userIsAuthenticated = false;
       }
     }
     );
-    this.usernameSubs=this.auth.getUsername().subscribe({
-      next :(username)=>{
-        this.username=username;
+    this.usernameSubs = this.auth.getUsername().subscribe({
+      next: (username) => {
+        this.username = username;
       },
-      error :()=>{
-        this.username="";
+      error: () => {
+        this.username = "";
       }
     });
 
-    this.roleSubs=this.auth.getRole().subscribe({
-      next :(role)=>{
-        this.role=role;
+    this.roleSubs = this.auth.getRole().subscribe({
+      next: (role) => {
+        this.role = role;
       },
-      error :()=>{
-        this.role=""
+      error: () => {
+        this.role = ""
       }
     })
-    console.log("username",this.username);
+
+    this.token=this.auth.getToken();
+    this.decodedToken = jwt_decode(this.token) as DecodedToken;
+    this.userId=this.decodedToken.id;
+    console.log("user ID", this.userId);
   }
 
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   this.username = this.auth.getUsername();
-  //   this.role=this.auth.getRole();
-  // }
+  profile(){
 
+  }
 
 
   ngOnDestroy(): void {
@@ -64,3 +71,4 @@ export class HeaderComponent implements OnInit, OnDestroy  {
     this.auth.logout();
   }
 }
+
