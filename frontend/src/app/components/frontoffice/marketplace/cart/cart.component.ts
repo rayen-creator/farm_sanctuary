b400e46923245c29eead6949e8f34216601cc21b
ghaith <ghaith.benrzig@esprit.tn>
2023-04-09 23:55:40 +0100

    cart fixes

--- frontend/src/app/components/frontoffice/marketplace/cart/cart.component.ts
+++ frontend/src/app/components/frontoffice/marketplace/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CartService} from "../../../../core/services/cart.service";
 import {Product} from "../../../../core/models/product";
+import Swal from "sweetalert2";
+
 
 @Component({
   selector: 'app-cart',
@@ -16,5 +18,21 @@ export class CartComponent implements OnInit {
     this.cartProducts = this.cartService.getItems()
     console.log(this.cartProducts)
   }
-
+  clearCart() {
+    Swal.fire({
+      title: 'Are you sure you want to clear the cart?',
+      text: 'This action cannot be undone.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, clear cart',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.cartService.clearCart();
+        this.cartProducts = [];
+        Swal.fire('Cleared', 'Cart has been cleared successfully.', 'success');
+      }
+    });
+  }
 }
