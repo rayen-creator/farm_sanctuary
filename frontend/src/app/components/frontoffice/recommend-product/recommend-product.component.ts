352b63d911ce4699afda7a878432bb0603df71b0
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-04-25 18:32:38 +0100

    fixing webscraping

--- frontend/src/app/components/frontoffice/recommend-product/recommend-product.component.ts
+++ frontend/src/app/components/frontoffice/recommend-product/recommend-product.component.ts
@@ -1,17 +1,16 @@
 import { recommendedProductService } from './../../../core/services/recommendedProduct.service';
-import {AfterContentInit, Component} from '@angular/core';
+import {AfterContentInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { GET_PRODUCTS_QUERY_INPUTS, GET_PRODUCTS_QUERY_TYRES, GET_PRODUCTS_QUERY_WORKSHOP } from 'src/app/core/graphql/queries/graphql.queries.recommended';
 import { RecommendedProduct } from 'src/app/core/models/recommandedprod';
-import { interval, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-recommend-product',
   templateUrl: './recommend-product.component.html',
   styleUrls: ['./recommend-product.component.css']
 })
-export class RecommendProductComponent implements AfterContentInit {
+export class RecommendProductComponent implements OnInit {
   public recommendedproductsList: RecommendedProduct[];
   public visibleRecommendedProducts: RecommendedProduct[];
   public numberOfRecommendedProductsToShow = 9;
@@ -20,30 +19,15 @@ export class RecommendProductComponent implements AfterContentInit {
   productstyres: any[];
   productsworkhop: any[];
   productsinputs: any[];
-  destroy$: Subject<void> = new Subject();
 
 
 
-  constructor(private recommendedProductService: recommendedProductService,private apollo : Apollo) {
+  constructor(private recommendedProductService: recommendedProductService,private apollo : Apollo, private cd: ChangeDetectorRef) {
     
    }
 
-  ngAfterContentInit(): void {
-    this.apollo
-    .query({ query: GET_PRODUCTS_QUERY_TYRES })
-    .subscribe((result: any) => {
-      this.productstyres = result.data.productstyres;
-    });
-    this.apollo
-    .query({ query: GET_PRODUCTS_QUERY_WORKSHOP })
-    .subscribe((result: any) => {
-      this.productsworkhop = result.data.productsworkhop;
-    });
-    this.apollo
-    .query({ query: GET_PRODUCTS_QUERY_INPUTS })
-    .subscribe((result: any) => {
-      this.productsinputs = result.data.productsinputs;
-    });
+  ngOnInit(): void {
+    
     this.getAllRecommendedProducts();
     this.setupSorting();
 }
