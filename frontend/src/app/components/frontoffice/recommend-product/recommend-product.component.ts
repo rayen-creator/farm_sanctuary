693808132a41bd3eb74a0e53f4afa967f1799a19
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-04-25 14:07:33 +0100

    fixing recommended prod bug

--- frontend/src/app/components/frontoffice/recommend-product/recommend-product.component.ts
+++ frontend/src/app/components/frontoffice/recommend-product/recommend-product.component.ts
@@ -1,6 +1,10 @@
 import { recommendedProductService } from './../../../core/services/recommendedProduct.service';
 import {AfterContentInit, Component} from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { GET_PRODUCTS_QUERY_INPUTS, GET_PRODUCTS_QUERY_TYRES, GET_PRODUCTS_QUERY_WORKSHOP } from 'src/app/core/graphql/queries/graphql.queries.recommended';
 import { RecommendedProduct } from 'src/app/core/models/recommandedprod';
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recommend-product',
@@ -12,12 +16,44 @@ export class RecommendProductComponent implements AfterContentInit {
   public visibleRecommendedProducts: RecommendedProduct[];
   public numberOfRecommendedProductsToShow = 9;
   searchText: any;
-  constructor(private recommendedProductService: recommendedProductService) { }
+  products:any[];
+  productstyres: any[];
+  productsworkhop: any[];
+  productsinputs: any[];
+  destroy$: Subject<void> = new Subject();
+
+
+
+  constructor(private recommendedProductService: recommendedProductService,private apollo : Apollo) {
+    
+   }
 
   ngAfterContentInit(): void {
+    this.apollo
+    .query({ query: GET_PRODUCTS_QUERY_TYRES })
+    .subscribe((result: any) => {
+      this.productstyres = result.data.productstyres;
+    });
+    this.apollo
+    .query({ query: GET_PRODUCTS_QUERY_WORKSHOP })
+    .subscribe((result: any) => {
+      this.productsworkhop = result.data.productsworkhop;
+    });
+    this.apollo
+    .query({ query: GET_PRODUCTS_QUERY_INPUTS })
+    .subscribe((result: any) => {
+      this.productsinputs = result.data.productsinputs;
+    });
     this.getAllRecommendedProducts();
     this.setupSorting();
-  }
+}
+
+
+
+
+
+   
+  
   getAllRecommendedProducts(){
     this.recommendedProductService.getRecommendedProducts().subscribe({
       next: (products) => {
