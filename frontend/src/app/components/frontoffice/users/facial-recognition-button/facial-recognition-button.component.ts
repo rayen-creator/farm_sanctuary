52638e2748d417a4f99f40c4d4499983489ac483
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-05-01 20:55:29 +0100

    ✅ complete the UI for facial recognition

--- frontend/src/app/components/frontoffice/users/facial-recognition-button/facial-recognition-button.component.ts
+++ frontend/src/app/components/frontoffice/users/facial-recognition-button/facial-recognition-button.component.ts
@@ -1,7 +1,7 @@
 import { FacialRecognitionService } from './../../../../core/services/facial-recognition.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import {  ToastrService } from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
 import { WebcamImage } from 'ngx-webcam';
 import { Observable, Subject } from 'rxjs';
 
@@ -11,6 +11,7 @@ import { Observable, Subject } from 'rxjs';
   styleUrls: ['./facial-recognition-button.component.css']
 })
 export class FacialRecognitionButtonComponent implements OnInit {
+  @Input() username: string;
   selectedFile: any;
   imagePreview: string;
   webcamImageCaptured: boolean = false;
@@ -19,7 +20,7 @@ export class FacialRecognitionButtonComponent implements OnInit {
   constructor(
     private sanitizer: DomSanitizer,
     private FacialRecognitionService: FacialRecognitionService,
-    private toaster:ToastrService
+    private toaster: ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -27,10 +28,24 @@ export class FacialRecognitionButtonComponent implements OnInit {
   triggerSnapshot(): void {
     this.trigger.next();
   }
+
+  private dataURItoBlob(dataURI: string): Blob {
+    // Convert data URI to Blob object
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: mimeString });
+  }
   handleImage(webcamImage: WebcamImage): void {
     // console.info('Saved webcam image', webcamImage.imageAsDataUrl);
     this.webcamImageCaptured = true;
     this.webcamImage = webcamImage;
+
+
   }
 
   public get triggerObservable(): Observable<void> {
@@ -56,12 +71,31 @@ export class FacialRecognitionButtonComponent implements OnInit {
       const reader = new FileReader();
       reader.readAsDataURL(this.selectedFile);
       reader.onload = () => {
-       
+        // this.FacialRecognitionService.addFaceID().subscribe({
+        //   next:()=>{
+        //     this.toaster.success("picture saved !","success")
+        //   },
+        //   error:(err)=>{
+        //     throw err;
+        //   }
+        // })
 
       };
-    }else if (this.webcamImageCaptured){
-     
-    }
+    } else if (this.webcamImageCaptured) {
+      const imageData = this.dataURItoBlob(this.webcamImage.imageAsDataUrl);
+      // Create a FormData object to send the image data in the request body
+      const formData = new FormData();
+      formData.append('image', imageData, `${this.username}.jpg`);
 
+      // console.log("webcamImage", formData.get('image'));
+      this.FacialRecognitionService.addFaceID(formData).subscribe({
+        next: () => {
+          this.toaster.success("picture saved !", "success")
+        },
+        error: (err) => {
+          throw err;
+        }
+      })
+    }
   }
 }
